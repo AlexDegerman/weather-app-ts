e220a7dcb1438112f1296b1947b39f843cc294ef
AlexDegerman <alex.degerman.dev@gmail.com>
2025-05-09 02:03:24 +0300

    Connect to weatherapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,53 @@
+import { useState } from 'react'
 import './App.css'
+import axios from 'axios'
+
+interface WeatherData {
+  temp_c: number
+  condition: {
+    text: string
+    icon: string
+  }
+  humidity: number
+  wind_kph: number
+}
+
+interface ForecastDay {
+  date: string
+  day: {
+    avgtemp_c: number
+    condition: {
+      text: string
+      icon: string
+    }
+  }
+}
 
 const App = () => {
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null)
+  const [forecast, setForecast] = useState<ForecastDay[] | undefined>(undefined)
 
+  const fetchWeather = async (city: string) => {
+    try {
+      const apiKey = import.meta.env.VITE_API_KEY
+      const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=4&aqi=no&alerts=no`
+
+      const response = await axios.get(url)
+      setWeatherData(response.data.current)
+      setForecast(response.data.forecast.forestcastday)
+    } catch {
+      setWeatherData(null)
+    }
+  }
 
   return (
-    <div></div>
+    <div>
+      <h1>Weather App</h1>
+      <p>search bar</p>
+      {weatherData && (
+        <p>weather cards</p>
+      )}
+    </div>
   )
 }
 
