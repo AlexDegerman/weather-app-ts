570ae10c11b77489df9eb38f2d6dee989b68870a
AlexDegerman <alex.degerman.dev@gmail.com>
2025-05-09 02:37:32 +0300

    Prepare for deploy

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -21,7 +21,12 @@ const SearchBar = ({ onSearch }: SearchBarProps) => {
       placeholder="Enter city name"
       value={city}
       onChange={(e) => setCity(e.target.value)}
-      />
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          handleSearch()
+        }
+      }}
+    />
       <button onClick={handleSearch}>Search</button>
     </div>
   )
